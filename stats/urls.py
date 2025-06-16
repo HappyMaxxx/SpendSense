2fae55155b5db725305fcf6c6d393a6734eb4acf
v1mer <mpatik2006@gmail.com>
2025-06-16 12:52:37 +0300

    pie chart

--- stats/urls.py
+++ stats/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('reports/', views.reports_view, name='reports'),
+    path('chart-data/', views.chart_data, name='chart_data')
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from finance.views import LoginRequiredMixin
+from finance.models import Spents, Earnings
+from datetime import datetime, timedelta
+from django.http import JsonResponse
+from django.db.models import Sum
+from django.contrib import messages
 
 @login_required
 def reports_view(request):
-    # TODO
     return render(request, 'stats/reports.html')
+
+def chart_data(request):
+    period = request.GET.get('period', 'month')
+    now = datetime.now()
+
+    if period == '30days':
+        date_from = now - timedelta(days=30)
+    elif period == 'year':
+        date_from = now - timedelta(days=365)
+    else:  # month
+        date_from = now.replace(day=1)
+
+    spents = Spents.objects.filter(user=request.user, time_create__gte=date_from).values('category', 'amount')
+
+    category_totals = {}
+    for item in spents:
+        category = item['category']
+        amount = item['amount'].to_decimal()
+        if category in category_totals:
+            category_totals[category] += amount
+        else:
+            category_totals[category] = amount
+
+    labels = list(category_totals.keys())
+    data = list(category_totals.values())
+
+    return JsonResponse({'labels': labels, 'data': data})
